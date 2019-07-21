d5a46de5da2c97e8fd8d1506a97ad37666069532
Raymond Martinez <rcmartinez1854@gmail.com>
2019-07-20 18:06:43 -0700

    added submit function that appends the new shopping item

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+// defining my function inside of jQuery's callback syntax
+// where the code wont run until page has loaded.
+// this takes place of document.ready()
+
 $(function() {
-    // $('.shopping-item-toggle').submit(event => {
-    //     event.preventDefault();
-    //     const textElement = $(event.currentTarget).find('#shopping-list-entry');
-    //     $('.')
-    // });
+    
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        const textElement = $(event.currentTarget).find('#shopping-list-entry').val();
+        console.log(textElement);
+
+        $('#shopping-list-entry').val("");
+        $('.shopping-list').append(
+            `<li>
+                <span class="shopping-item">${textElement}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`
+        );
+    });
 
     $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
         event.preventDefault();
