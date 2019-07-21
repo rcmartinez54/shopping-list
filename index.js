d6c3e840d46eebd025fdf79961e2f1e82d494d47
Raymond Martinez <rcmartinez1854@gmail.com>
2019-07-20 17:42:27 -0700

    added toggle and delete functions for corresponding buttons

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 $(function() {
-    console.log('hello');     
+    // $('.shopping-item-toggle').submit(event => {
+    //     event.preventDefault();
+    //     const textElement = $(event.currentTarget).find('#shopping-list-entry');
+    //     $('.')
+    // });
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        event.preventDefault();
+        $(this).closest('li').remove();
+    });
+        
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        event.preventDefault();
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
 });
